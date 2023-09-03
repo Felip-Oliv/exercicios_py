bbdd48f5cf1580f1d25d535c8be4702eff6014d8
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-09-03 12:20:26 -0300

    Update termmoo

--- lista_exercicios/3/termoo.py
+++ lista_exercicios/3/termoo.py
@@ -1,13 +1,34 @@
 import os
 import random
-print(os.getcwd())
-
 # início do programa
 
-arquivo = "lista_palavras.txt" # altere o caminho se necessário
-                               # o ideal é que esteja no mesmo diretório do programa
+arquivo = 'C:/_src/exercicios_py/lista_exercicios/3/lista_palavras.txt' # altere o caminho se necessário
+# o ideal é que esteja no mesmo diretório do programa
                                
 def le_arquivo(arq):
     """ Lê arquivo especificado e retorna uma lista com todas as linhas """    
     with open(arq, encoding="UTF-8") as f:
         return [linha.strip() for linha in f] # método strip remove o '\n' do final da linha
+    
+    
+def gera_lista_n_letras(lista, n):
+    return [x for x in lista if len(x) == n]
+
+def imprime_palavra_atual(palavra):
+    for l in palavra:
+        print(l,end=" ")
+    print("")
+
+lista = le_arquivo(arquivo)
+print(lista) # descomente para verificar se a lista está correta
+
+lista = le_arquivo(arquivo)
+# print(lista) # descomente para verificar se a lista está correta
+
+
+qtde = int(input("Quantas letras? "))
+lista_n = gera_lista_n_letras(lista, qtde)
+
+print(lista_n)
+palavra = list(random.choice(lista_n))
+print(palavra)
