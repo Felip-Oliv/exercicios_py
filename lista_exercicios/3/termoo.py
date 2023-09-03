8cf234a9dd37ea6eca13033eee4310b94ae9a85c
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-09-03 12:07:22 -0300

    Uptade termoo
    lendo o arquivo e formatando o mesmo

--- lista_exercicios/3/termoo.py
+++ lista_exercicios/3/termoo.py
@@ -1,3 +1,13 @@
 import os
 import random
 print(os.getcwd())
+
+# início do programa
+
+arquivo = "lista_palavras.txt" # altere o caminho se necessário
+                               # o ideal é que esteja no mesmo diretório do programa
+                               
+def le_arquivo(arq):
+    """ Lê arquivo especificado e retorna uma lista com todas as linhas """    
+    with open(arq, encoding="UTF-8") as f:
+        return [linha.strip() for linha in f] # método strip remove o '\n' do final da linha
